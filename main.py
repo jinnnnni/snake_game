3ea027df5106a52cbe86e70e53fb492bdffcf13b
jinnnnni <keg911016@gmail.com>
2024-05-11 10:38:35 +0900

    화면 만들기

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle  
+
+# turtle 은 모듈, Turtle 은 클래스
+
+screen = Screen()
+screen.setup(width="600", height="600")
+screen.bgcolor("white")
+screen.title("My Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
