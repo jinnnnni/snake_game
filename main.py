3e463415c64381acd55ba33a0c6c78fdbc7332d0
jinnnnni <keg911016@gmail.com>
2024-05-11 11:06:47 +0900

    거북이 몸통 반복문으로 만들기

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+#튜플
 
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
-#거북이 만들 때 모양 명시할 수 있음
-
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
-#두번째, 세번째 거북이의 좌표는 -20 씩 옆으로 이동해야함.
-
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
+for position in starting_position:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
