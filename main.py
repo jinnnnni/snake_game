241974e28afc4d331fad590334cc9aedb19799a0
jinnnnni <keg911016@gmail.com>
2024-05-11 12:44:37 +0900

    거북이 움직이기

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ screen.title("My Snake Game")
 
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
 #튜플
+segments=[]
 
 for position in starting_position:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup() #펜을 들지 않으면 밑으로 밑줄이 그어짐    
     new_segment.goto(position)
+    segments.append(new_segment) #배열에 담아줌    
 
 
+game_is_on = True #게임이 동작하는 지 결정
 
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)  #거북이 배열이 하나씩 앞으로(오른쪽으로) 20씩 움직임 
 
 
 
