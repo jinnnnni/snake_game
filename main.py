3c6672493eed255623b96ac0945daee35eba411b
jinnnnni <keg911016@gmail.com>
2024-05-11 13:22:53 +0900

    거북이 회전하는데 뒤에 세그먼트들도 따라오게 하기

--- main.py
+++ main.py
@@ -24,13 +24,15 @@ game_is_on = True #게임이 동작하는 지 결정
 while game_is_on:
     screen.update()    #모든 세그먼트가 이동한 후 화면이 갱신됨
     time.sleep(0.1)    #모든 세그먼트가 이동한 후 시간 지연(좀더 빨라짐 -> 더빠르게 는 0.1로)
-    for seg in segments:
-        seg.forward(20)  #거북이 배열이 하나씩 앞으로(오른쪽으로) 20씩 움직임 
-           
-    segments[0].left(20) #거북이의 첫번째 세그먼트를 왼쪽으로 20만큼 돌림
+    
+    for seg_num in range(len(segments) - 1, 0, -1): #range() 함수는 순수함수가 아니기 때문에 키워드 인자를 넣으면 에러가 남
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        # 세번째 세그먼트를 두번째 위치로 보낼거기 떄문에 두번째 세그먼트의 위치좌표를 구해줌
+        segments[seg_num].goto(new_x, new_y)
 
-
-
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 
